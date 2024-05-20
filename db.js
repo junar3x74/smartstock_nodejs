4cd50d4787926795f1d22981d9ab9bf3ea10ad91
junar3x74 <junarafable14@gmail.com>
2024-05-19 20:33:33 -0700

    use env file for database connections

--- db.js
+++ db.js
@@ -1,10 +1,12 @@
 const mysql = require("mysql");
+const dotenv = require("dotenv");
+dotenv.config({ path: "./config.env" });
 
 const db = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password:'',
-    database: 'smartstock_db'
+    host: process.env.DATABASE_HOST,
+    user: process.env.DATABASE_USER,
+    password:process.env.DATABASE_PASSWORD,
+    database: process.env.DATABASE
 })
 
 
